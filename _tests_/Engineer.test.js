d3ba516c5fe678c97793d5bd2db9befa3a339bc2
PacDraft <Kirsten@PacificDraftWorks.com>
2022-02-01 19:07:15 -0800

    finished engineer tests

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -6,13 +6,15 @@ test("GitHub via constructor", () => {
   const engineer = new Engineer("Tom", 46, "tom@test.com", testData);
   expect(engineer.employeeGitHub).toEqual(testData);
 });
-//working here
+
+// Test using role
 test("getRole equals engineer", () => {
   const testData = "Engineer";
   const engineer = new Engineer("Tom", 46, "tom@test.com", "GitHubUser");
   expect(engineer.getRole()).toEqual(testData);
 });
 
+// Test using getEmployeeGitHub
 test("username via GitHub", () => {
   const testData = "GitHubUser";
   const engineer = new Engineer("Tom", 46, "tom@test.com", testData);
