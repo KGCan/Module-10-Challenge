25b1a2473ff20412ff04664cf124e692cef613ba
PacDraft <Kirsten@PacificDraftWorks.com>
2022-02-21 10:42:30 -0800

    corrected error in index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Engineer = require ("./lib/Engineer");
 const Intern = require ("./lib/Intern");
 
 //Modules & packages
-const inquirer = require (inquirer);
+const inquirer = require ("inquirer");
 const fs = require ("fs");
 const path = require ("path");
 
